6b5bff1e32db42eca52d5deb5dc9df36d8a8bde8
AtifNaeem1 <100839831+AtifNaeem1@users.noreply.github.com>
2022-04-28 12:22:35 +0530

    Add files via upload

---
+++ SpringAOP/springDemoAOP-pointcut-order-aspect/src/main/java/aopDemo/MainDemoApp.java
@@ -0,0 +1,38 @@
+package aopDemo;
+
+import aopDemo.dao.AccountDAO;
+import aopDemo.dao.MembershipDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        //get the bean from spring container
+        AccountDAO theAccountDao = context.getBean("accountDAO", AccountDAO.class);
+
+        //get membership bean from container
+        MembershipDAO theMembershipDAO = context.getBean("membershipDAO",MembershipDAO.class);
+
+
+        //call the business method
+        theAccountDao.addAccount(new Account());
+
+        //call getter and setter
+        theAccountDao.setName("Ajay");
+        theAccountDao.setServiceCode("silver");
+
+        String name = theAccountDao.getName();
+        String code = theAccountDao.getServiceCode();
+
+        //call the membership business method
+        theMembershipDAO.addSillyMember();
+
+
+        //close the context
+        context.close();
+    }
+}
---
+++ SpringAOP/springDemoAOP-pointcut-order-aspect/src/main/java/aopDemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,20 @@
+package aopDemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+    @Before("aopDemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice()
+    {
+        System.out.println("\n====>> Executing @Before advice on addAccount()");
+    }
+
+}
